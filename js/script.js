2d1e395e99500e7840f9a22a91a8f6b4e772f22f
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-05-12 11:36:16 +0600

    adding order system

--- js/script.js
+++ js/script.js
@@ -2,3 +2,8 @@ function showDeliveryAddress() {
     var x = document.getElementById("deliveryAddress");
     x.style.display = "block";
 }
+
+function hideDeliveryAddress() {
+    var x = document.getElementById("deliveryAddress");
+    x.style.display = "none";
+}
