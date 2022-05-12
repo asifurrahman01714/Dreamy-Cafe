8543361e9167a07b48372fe5845092f35330cd08
asifurrahman01714 <asif.eee.cu@gmail.com>
2022-05-12 12:34:12 +0600

    same as delivery address

--- js/script.js
+++ js/script.js
@@ -7,3 +7,34 @@ function hideDeliveryAddress() {
     var x = document.getElementById("deliveryAddress");
     x.style.display = "none";
 }
+
+function validateRegistrationForm() {
+    // Input Name
+    let name = document.forms["registrationForm"]["name"].value;
+    if (name == "") {
+        alert("Name must be filled out");
+        return false;
+    }
+
+    // Password Validation
+    let password = document.forms["registrationForm"]["password"].value;
+    if (password == "") {
+        alert("Password must be filled out"); 
+        return false;
+    }
+    if (password.length < 7) {
+        alert("Password must be 7 characters long"); 
+        return false;
+    }
+
+}
+
+function showPaymentInformation() {
+    var x = document.getElementById("payment");
+    x.style.display = "block";
+}
+
+function hidePaymentInformation() {
+    var x = document.getElementById("payment");
+    x.style.display = "none";
+}
